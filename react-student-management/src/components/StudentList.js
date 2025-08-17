bddd32ee6eb22217940aba28e0c7a760d0188f7f
Long <phambaolong10031997@gmail.com>
2025-08-17 17:35:49 +0800

    new

--- react-student-management/src/components/StudentList.js
+++ react-student-management/src/components/StudentList.js
@@ -138,7 +138,7 @@ const StudentList = () => {
         ) : (
           <div>
             <br />
-            <p>Please click on a Student...New version-V1.0.2</p>
+            <p>Please click on a Student...New version-V1.0.3</p>
           </div>
         )}
       </div>
